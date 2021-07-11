653c033ccb8db1dd54b47524a4ddd2ffa81efe97
korposdavid <korposdavid@gmail.com>
2021-07-11 09:43:11 +0200

    Initial commit

---
+++ src/main/java/hu/korposdavid/emagtoolsapi/EmagToolsApiApplication.java
@@ -0,0 +1,13 @@
+package hu.korposdavid.emagtoolsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmagToolsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmagToolsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/korposdavid/emagtoolsapi/EmagToolsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.korposdavid.emagtoolsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmagToolsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
